83cffc59d4f5b2f1b9efcd495389f08497d64a01
AndyGuit <totalslash@msn.com>
2021-06-28 16:32:04 +0300

    feat: css module for card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,7 +1,10 @@
+import styles from './Card.module.scss';
+
 function Card(props) {
+
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
       <img width={133} height={112} src={props.imageUrl} alt=""/>
@@ -11,7 +14,7 @@ function Card(props) {
           <span>Цена:</span>
           <b>{props.price} руб.</b>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img width={11} height={11} src="/img/plus.svg" alt="button plus" />
         </button>
       </div>
